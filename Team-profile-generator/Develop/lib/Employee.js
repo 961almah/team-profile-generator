3e2c3dc1d1401f96f8972c986fd4e44dc40e06e0
Ali Mahmoud <961alimahmoud@gmail.com>
2020-10-31 12:25:03 -0400

    created employee class constructor

---
+++ Team-profile-generator/Develop/lib/Employee.js
@@ -0,0 +1,39 @@
+// TODO: Write code to define and export the Employee class
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee.
+
+// TODO: Write code to define and export the Employee class
+
+// The first class is an Employee parent class
+class Employee {
+    // Reference for constructors within classes: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/constructor
+    // Set up base level parameters in which user can input data to
+    constructor(name, id, email) {
+    // Parent class requires the following properties and methods:
+        // Name
+        this.name = name;
+        // ID
+        this.id = id;
+        // Email
+        this.email = email;
+    }
+    // getName()
+    getName() {
+        return this.name;
+    }
+    // getId()
+    getId() {
+        return this.id;
+    }
+    // getEmail()
+    getEmail() {
+        return this.email;
+    }
+    // getRole() // returns 'Employee'
+    getRole() {
+        return 'Employee';
+    }
+}
+
+// Export module
+module.exports = Employee;
