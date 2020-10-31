ff84963d721b623d69abc4a825dd5db79c40a2e5
Ali Mahmoud <961alimahmoud@gmail.com>
2020-10-31 12:58:40 -0400

    created intern class extensions

--- Team-profile-generator/Develop/lib/Intern.js
+++ Team-profile-generator/Develop/lib/Intern.js
@@ -1,1 +1,26 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require('./Employee');
+// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+// Require employee
+const Employee = require('./Employee');
+
+// Create intern class that extends employee
+class Intern extends Employee {
+    // Create constructor with parameters from employee, as well as school
+    constructor(name,  id, email, school) {
+        super(name, id, email);
+        this.school = school; 
+    }
+    // getSchool()
+    getSchool() {
+        return this.school;
+    }
+    // getRole() // overridden to return intern
+    getRole() {
+        return 'Intern';
+    }
+}
+
+// Export module
+module.exports = Intern;
